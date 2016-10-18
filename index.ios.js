15a86d468f0866980e2daa20e1bbecccda579dc3
Admin <admin@Admins-Mac-Pro-2.local>
2016-10-18 19:09:14 +0530

    init commit

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,42 +6,58 @@ import {
   View
 } from 'react-native';
 
-export default class thedal extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import {Scene, Router} from 'react-native-router-flux';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import styles from './app/styles.js';
+
+
+class tempview extends Component{
+  render() {
+    return <ScrollableTabView
+      style={{marginTop: 20, }}
+      initialPage={1}
+      renderTabBar={() => <FacebookTabBar />}
+      >
+      <ScrollView tabLabel="ios-paper" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>News</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-people" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Friends</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-chatboxes" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Messenger</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-notifications" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Notifications</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-list" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Other nav</Text>
+        </View>
+      </ScrollView>
+    </ScrollableTabView>;
+  }
+} 
+
+
+export default class thedal extends Component {
+    render() {
+    return <Router>
+      <Scene key="root">
+        <Scene key="tempview" component={tempview} title="tempview"/>
+      </Scene>
+    </Router>
+  }
+};
+
 
 AppRegistry.registerComponent('thedal', () => thedal);
